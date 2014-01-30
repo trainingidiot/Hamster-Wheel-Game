1b36d78c7daa853e994e95dd72038129b08113a7
Jesse Huff <jesse@jessehuff.com>
2014-01-30 12:38:04 -0800

    physics stuff

--- src/GameLevel.java
+++ src/GameLevel.java
@@ -1,3 +1,13 @@
+
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.collision.shapes.ShapeType;
+import org.jbox2d.common.MathUtils;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.World;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -5,14 +15,25 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.newdawn.slick.geom.Transform;
+
+
 
 
 public class GameLevel extends BasicGameState {
 
 	Animation sprite, left, right, leftStill, rightStill;
 	Image board, wheel;
+    private static final World world = new World(new Vec2(0, -9.8f));
+    int velocityIterations;
+    int positionIterations;
+    float pixelsPerMeter;
+
+
 	
 	private int state;
 	
@@ -22,6 +43,7 @@ public class GameLevel extends BasicGameState {
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		gc.setShowFPS(false);
+
 		
 		//Image [] movementLeft =  {new Image("images/Biped/drag_wr_0.png"), new Image("images/Biped/drag_wr_1.png"), new Image("images/Biped/drag_wr_2.png"), new Image("images/Biped/drag_wr_3.png"),new Image("images/Biped/drag_wr_4.png"), new Image("images/Biped/drag_wr_5.png"), new Image("images/Biped/drag_wr_6.png"), new Image("images/Biped/drag_wr_7.png"),new Image("images/Biped/drag_wr_8.png"), new Image("images/Biped/drag_wr_9.png"), new Image("images/Biped/drag_wr_10.png"), new Image("images/Biped/drag_wr_11.png")} ;
         //Image [] movementRight =  {new Image("images/Biped/drag_wl_0.png"), new Image("images/Biped/drag_wl_1.png"), new Image("images/Biped/drag_wl_2.png"), new Image("images/Biped/drag_wl_3.png"),new Image("images/Biped/drag_wl_4.png"), new Image("images/Biped/drag_wl_5.png"), new Image("images/Biped/drag_wl_6.png"), new Image("images/Biped/drag_wl_7.png"),new Image("images/Biped/drag_wl_8.png"), new Image("images/Biped/drag_wl_9.png"), new Image("images/Biped/drag_wl_10.png"), new Image("images/Biped/drag_wl_11.png")} ;
@@ -37,17 +59,101 @@ public class GameLevel extends BasicGameState {
         //leftStill = new Animation(movementLeftStill, durationStill, true);
         //rightStill = new Animation(movementRightStill, durationStill, true);
         
+        velocityIterations = 10;
+        positionIterations = 10;
+        pixelsPerMeter = 30.f;
+        
+        {
+            BodyDef dground = new BodyDef();
+            dground.active = true;
+            dground.position = new Vec2(0.f, 0.f);
+            dground.type = BodyType.STATIC;
+            Body bground = world.createBody(dground);
+            PolygonShape sground = new PolygonShape();
+            sground.setAsBox(20f, 0.5f);
+            bground.createFixture(sground, 1000.f);
+        }
+        {
+            BodyDef dbox = new BodyDef();
+            dbox.active = true;
+            dbox.position = new Vec2(0.f, 10.f);
+            dbox.angle = MathUtils.PI / 3.f;
+            dbox.type = BodyType.DYNAMIC;
+            Body bbox = world.createBody(dbox);
+            PolygonShape sbox = new PolygonShape();
+            Vec2[] data = 
+            {
+                new Vec2(-0.3f, -0.7f),
+                new Vec2(0.0f, -1.0f),
+                new Vec2(0.3f, -0.7f),
+                new Vec2(0.5f, 0.5f),
+                new Vec2(-0.5f, 0.5f)
+            };
+            sbox.set(data, 4);
+            sbox.setAsBox(0.5f, 0.5f);
+            bbox.createFixture(sbox, 1.f);
+            bbox.setAngularVelocity(10.f);
+        }
+        
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		 g.setBackground(Color.white);
+		 //g.setBackground(Color.white);
          board.draw(-1, -1);
          wheel.draw(-1,400);
          
+         
+         Body current = world.getBodyList();
+         Vec2 center = current.getLocalCenter();
+         while(current != null)
+         {
+             Vec2 pos = current.getPosition();
+             g.pushTransform();
+             g.translate(pos.x * pixelsPerMeter + (0.5f * gc.getWidth()), 
+                        -pos.y * pixelsPerMeter + (0.5f * gc.getHeight()));
+             Fixture f = current.getFixtureList();
+             while(f != null)
+             {
+                 ShapeType type = f.getType();
+                 switch(type)
+                 {
+                     case POLYGON:
+                     {
+                         PolygonShape shape = (PolygonShape)f.getShape();
+                         Vec2[] verts = shape.getVertices();
+                         int count = shape.getVertexCount();
+                         Polygon p = new Polygon();
+                         for(int i = 0; i < count; i++)
+                         {
+                             p.addPoint(verts[i].x, verts[i].y);
+                         }
+                         p.setCenterX(center.x);
+                         p.setCenterY(center.y);
+                         p = (Polygon)p.transform(Transform.createRotateTransform(current.getAngle() + MathUtils.PI, center.x, center.y));
+                         p = (Polygon)p.transform(Transform.createScaleTransform(pixelsPerMeter, pixelsPerMeter));
+                         g.draw(p);
+                         break;
+                     }
+                     case CIRCLE:
+                     {
+                         CircleShape shape = (CircleShape)f.getShape();
+                     }
+                     default:
+                 }
+
+                 f = f.getNext();
+             }
+             g.popTransform();
+             current = current.getNext();
+         }
+ 
 	}
 
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
-		 Input input = container.getInput();
+        world.step((float)delta / 1000.f, velocityIterations, positionIterations);
+
+		
+		Input input = container.getInput();
 
          if (input.isKeyDown(Input.KEY_LEFT)==false)
          {
@@ -61,11 +167,15 @@ public class GameLevel extends BasicGameState {
          {
         	 sbg.enterState(0); //return to menu for now
          }
+         
 	}
 
 	public int getID() {
 		
 		return state;
 	}
+	
+
+
 
 }
