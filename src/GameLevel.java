63d6b4515c53f274f533ff7d236558ecfa9ff47e
Kelly Yin <kyin2@uci.edu>
2014-03-17 20:37:48 -0700

    You have to press ENTER to make the droplets fall down.

--- src/GameLevel.java
+++ src/GameLevel.java
@@ -177,7 +177,7 @@ public class GameLevel extends BasicGameState {
 	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		System.out.println("enter");
 		
-		started = true;
+		started = false;
 		points = 500;
 		timedown = false;
 		
